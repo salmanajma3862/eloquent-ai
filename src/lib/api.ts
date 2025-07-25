636eb8188eab77a4bc85c96e6fff685d06f009fb
salmanajma3862 <salmanajmal412@gmail.com>
2025-07-25 23:56:46 +0500

    added ip address url instead of localhost

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'http://192.168.100.35:5000',
     headers: {
         'Content-Type': 'application/json',
     },
